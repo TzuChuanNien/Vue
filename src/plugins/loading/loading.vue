4b6b8e847ffc2a110b4b60223aa7cc3bc2f6c392
chuannien <chuan.nien@i-chainwin.com>
2022-01-20 00:22:38 +0800

    新增axios封裝

--- src/plugins/loading/loading.vue
+++ src/plugins/loading/loading.vue
@@ -1,28 +1,25 @@
 <template>
  <div class="flex justify-center items-center">
-  <div :status="value" z-index="9999" class="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full" role="status">
+  <div :value="value" z-index="9999" class="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full" role="status">
     <span class="visually-hidden">Loading...</span>
   </div>
 </div>
 </template>
 
 <script>
-import { ref} from 'vue'
+import { ref } from 'vue'
 export default {
   setup() {
-    const status=ref(false);
+    const value=ref(false);
     const show=()=>{
-        status.value = true;
+        value.value = true;
         }
     const hide=()=>{
-        status.value = false;
+        value.value = false;
         }
     return {
-    show,hide,status
+    show,hide,value
     }
   },
-  methods: {
-
-  }
 };
 </script>
